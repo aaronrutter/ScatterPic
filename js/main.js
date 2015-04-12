49b079aa6ff8aa02923ba46df875524075da7fa0
Aaron Rutter <aaronlrutter@gmail.com>
2015-04-11 23:44:48 -0400

    adding main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var MyGame = MyGame || {};
+
+MyGame.game = new Phaser.Game(1920, 1080, Phaser.AUTO, '');
+
+MyGame.game.state.add('Boot', MyGame.Boot);
+
+MyGame.game.state.add('MainMenu', MyGame.MainMenu);
+MyGame.game.state.add('Settings', MyGame.Settings);
+
+MyGame.game.state.add('Preload', MyGame.Preload);
+MyGame.game.state.add('GamePlay', MyGame.GamePlay);
+MyGame.game.state.add('GameOver', MyGame.GameOver);
+
+MyGame.game.state.start('Boot');
